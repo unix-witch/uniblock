af6e69f3806ebe477381367184a9e0002a1725b8
bsd_witch <77200446+unix-witch@users.noreply.github.com>
2022-03-07 09:09:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,31 @@
-//https://nodejs.taylorliang.repl.co/
-const express = require("express");
 const unblocker = require("unblocker");
-const bodyParser = require("body-parser")
+const express = require("express");
 
+let Unblocker = new unblocker({prefix: '/unblock/'});
 
-var app = express();
+let app = express();
+app.use(Unblocker);
+app.use(express.static('views'));
+app.use(express.urlencoded({extended: true}));
 
-/*
-var ip = req.headers['x-forwarded-for'] || 
-    req.connection.remoteAddress || 
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-*/
-
-app.use(express.static('static'))
-app.use(new unblocker({'prefix':    '/pog/'}));
-app.use(new bodyParser.urlencoded({extended: true}));
-
-app.get('/', function(req, res){
-	res.sendFile("static/index.html", {root: __dirname});
+app.get('/', function(req, res) { 
+    res.sendFile("index.html"); 
 });
 
-app.post('/url', function(req, res){
-    
-    if (req.body.rurl){
+app.post('/site', function(req, res) {
+    if (req.body.url) {
+        if (/^(?:[a-z]+:)?\/\//i.test(req.body.url)) res.redirect('/unblock/https://'+req.body.url);
+        else res.redirect('/unblock/https://'+req.body.url.replace("https://", ""));
+
         
-        url = req.body.rurl;
-		key = req.body.key;
-
-		if (key === "pogchamp") { res.sendFile("static/no.html", {root: __dirname}); return; }
-		if ((key !== "password") && (key !== "stronk")) { res.sendFile("static/welcome.html", {root: __dirname}); return; } 
-
-        url = url.replace("https://", "").replace("http://", "");
-        
+    } else 
+        res.sendFile("no-url.html");
 
 
-        var ip = req.headers['x-forwarded-for'] || 
-            req.connection.remoteAddress || 
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress;
+    console.log(req.body);
+});
 
-        logString = `[${ip}]: ${url}\n`;
-        console.log(logString.replace("\n", ""), "w/", key);
-        
 
-        if (/^(?:[a-z]+:)?\/\//i.test(url)){
-            res.redirect('/pog/https://'+url);
-        } else {
-            res.redirect('/pog/https://'+url.replace("https://", ""));
-        }
-    }
-})
-
-app.listen(3000, () => console.log('uh oh stinky'));
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
